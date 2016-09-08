6993eca66fd914cb9483cb456143fd233a32edaf
Jonas Sideravičius <jonas.sideravicius@gmail.com>
2016-09-08 11:42:11 +0200

    simple test scene with terminal. circular buffer implementation added as well

---
+++ logo3d/Assets/Scripts/UI/TerminalController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TerminalController : MonoBehaviour {
+	
+	public InputField terminal;
+	public int commCount = 10; //sets the memorized command for up arrow count
+
+	CircularBuffer lastComm; //class found in Utils folder
+
+
+
+	// Use this for initialization
+	void Start () {
+		lastComm = new CircularBuffer(commCount);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			getLastCommand ();
+		}
+		if (Input.GetKeyDown (KeyCode.Return))
+			sendInput ();
+	}
+
+	public void sendInput(){
+		var temp = terminal.text; // clearing and using shorter var for convenience
+		terminal.text = "";
+		terminal.ActivateInputField ();
+		if (temp != "") {
+			lastComm.Push (temp);
+		}
+	}
+	void getLastCommand(){
+		terminal.text = lastComm.Peek ();
+	}
+}
+
---
+++ logo3d/Assets/Scripts/Utils/CircularBuffer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircularBuffer : MonoBehaviour {
+
+	public CircularBuffer(int count){
+		_max = count;
+		_top = 0;
+		_arr = new string[_max];
+		_length = 0;
+		_peeker = 0;
+
+	}
+	private int _top;
+	private int _length;
+	private int _peeker; //used to traverse the stack multiple times
+	private int _max;
+	private string[] _arr;
+	public void Push(string comm){
+		if (_top < _max - 1) {
+			_arr [_top] = comm;
+			_top++;
+		} else {
+			_top = 0;
+			_arr [_max - 1] = comm;
+		}
+		if (_length < _max) {
+			_length++;
+		}
+		_peeker = _top - 1;
+
+	}
+	public string Peek(){
+		if (_peeker <= -1) {
+			_peeker = _length - 1;
+		}
+		var temp = _peeker;
+		_peeker--;
+		return _arr [temp];
+	}
+}
